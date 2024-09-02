1aea6c8a223bdc434e44cf24b652891585ee76b3
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-09-02 17:23:17 +0300

    change_logic

--- main.py
+++ main.py
@@ -1,64 +1,54 @@
 import telebot
 import sqlite3
+from telebot import types 
 from datetime import datetime
-from telebot import types
 
 bot = telebot.TeleBot('7122079884:AAHXd98FRmrHMf7mqHwysxza0Hs_6930yWE')
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, f'Привет, {message.from_user.username}. \n\n Я бот созданный для помощи тебе в саморазвитии.')
-
-    current_date = datetime.now()
-    formatted_date = current_date.strftime("%d.%m.%Y")
-
-    check_user_on_db(message.from_user.id, message.from_user.username, formatted_date, message)
-    
-
-@bot.message_handler(func=lambda message: True)
-def add_on_db_message(message):
-    markup = types.InlineKeyboardMarkup()
+def start_message(message):
+    markup_create_table = types.InlineKeyboardMarkup()
     create_table = types.InlineKeyboardButton("Создать таблицу", callback_data='create_table')
-    markup.add(create_table)
-    bot.send_message(message.chat.id, 'Вижу ты новенький. Я добавил тебя в базу данных и предлагаю создать тебе свою первую таблицу с привычками.', reply_markup=markup)
+    markup_create_table.add(create_table)
 
-def old_user_message(message):
-    markup = types.InlineKeyboardMarkup()
+    markup_table_list = types.InlineKeyboardMarkup()
     table_list = types.InlineKeyboardButton("Список таблиц", callback_data='table_list')
-    markup.add(table_list)
-    bot.send_message(message.chat.id, 'Вижу ты уже работал со мной. Давай посмотрим на твои старые таблицы.', reply_markup=markup)
+    markup_table_list.add(table_list)
 
-def echo_all(message):
-    bot.reply_to(message, "Ваше сообщение сохранено в базе данных.")
+    user_id = message.from_user.id
+    username = message.from_user.username
+    current_date = datetime.now()
+    date = current_date.strftime("%d.%m.%Y")
 
-def check_user_on_db(user_id, username, date, message):
+    bot.send_message(message.chat.id, f'Привет {username}')
+
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
     cursor.execute('SELECT id FROM users')
     ids = cursor.fetchall()
     id_list = [row[0] for row in ids]
-    print(id_list)
-
-    conn.close()
 
     if user_id in id_list:
-        print('Пользователь найден')
-        old_user_message(message)
+        print(f'Пользователь найден: {user_id}')
+        bot.send_message(message.chat.id, 'Вижу мы уже с вами работали. Хотите продолжить?', reply_markup=markup_table_list)
     else:
-        print('Пользователь не найден')
-        add_user(user_id, username, date)
-        add_on_db_message(message)
+        print(f'Пользователь {user_id} не найден. Начинаю добавление в базу данных.')
+        cursor.execute(''' INSERT INTO users (id, username, date, tables) VALUES (?, ?, ?, ?)
+            ''', (user_id, username, date, 0))
 
-def add_user(id, username, date):
-    print(id, username, date, 0)
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
+        conn.commit()
+        print(f'Добавил пользователя {user_id} в базу данных.')
+        bot.send_message(message.chat.id, 'Первый раз пользуетесь ботом? Давай начнем с создания таблицы.', reply_markup=markup_create_table)
+    
+    conn.close()
 
-    cursor.execute('''
-    INSERT INTO users (id, username, date, tables) VALUES (?, ?, ?, ?)
-    ''', (id, username, date, 0))
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'create_table':
+        bot.send_message(call.message.chat.id, 'Давайте поподробнее расскажу про создание таблицы. \n\n Вот небольшой пример создания: \n day TEXT,\n reed_book INT,\n gym_time INT,\n sleep_time INT,')
+    elif call.data == 'table_list':
+        bot.send_message(call.message.chat.id, 'Вы выбрали список таблиц.')
 
-    conn.commit()
 
 bot.polling()
